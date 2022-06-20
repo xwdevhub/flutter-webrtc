85fee3d383b2d1e06bbbf3c023c6632c2a8f7e17
qiangjindong <qiangjindong@163.com>
2022-06-20 17:51:10 +0800

    feat: 优化iOS CVPixelBuffer数据传输

--- ios/Classes/ScreenCapture/XWRecvTansport.m
+++ ios/Classes/ScreenCapture/XWRecvTansport.m
@@ -20,7 +20,6 @@
     NSMutableArray<GCDAsyncSocket *> *_sockets;
     NSMutableData *_cacheData;
     dispatch_queue_t _queue;
-    dispatch_queue_t _videoProcessQueue;
 }
 
 - (instancetype)initWithHost:(NSString *)host port:(uint16_t)port {
@@ -29,29 +28,27 @@
         _port = port;
         _sockets = [NSMutableArray array];
         _cacheData = [NSMutableData data];
-        _videoProcessQueue = dispatch_queue_create("com.xanway.replay.recv.videoprocess", DISPATCH_QUEUE_SERIAL);
     }
     return self;
 }
 
-- (void)dealloc {
-    [self reset];
-}
-
 - (void)connect {
-    _queue = dispatch_queue_create("com.webrtc.replay.recv", DISPATCH_QUEUE_SERIAL);
+    //    _queue = dispatch_queue_create("com.webrtc.replay.recv", DISPATCH_QUEUE_SERIAL);
+    _queue = dispatch_get_main_queue();
     _socket = [[GCDAsyncSocket alloc] initWithDelegate:self delegateQueue:_queue];
     [_socket acceptOnPort:8999 error:nil];
     [_socket readDataWithTimeout:-1 tag:0];
 }
 
 - (void)reset {
-    if (_socket && _socket.isConnected) {
-        [_socket disconnect];
-        _socket = nil;
-    }
-    [_sockets removeAllObjects];
-    _cacheData = [NSMutableData data];
+    dispatch_async(_queue, ^{
+        if (self->_socket && self->_socket.isConnected) {
+            [self->_socket disconnect];
+            self->_socket = nil;
+        }
+        [self->_sockets removeAllObjects];
+        self->_cacheData = [NSMutableData data];
+    });
 }
 
 #pragma mark - GCDAsyncSocketDelegate
@@ -70,48 +67,46 @@
 }
 
 - (void)socket:(GCDAsyncSocket *)sock didReadData:(NSData *)data withTag:(long)tag {
-    dispatch_async(_videoProcessQueue, ^{
-        @autoreleasepool {
-            [self->_cacheData appendData:data];
+    @autoreleasepool {
+        [_cacheData appendData:data];
 
-            struct PixelBufferHead head;
-            memcpy(&head, self->_cacheData.bytes, sizeof(head));
-            //    NSLog(@">>>size %d, w %d, h %d", head.size, head.width, head.height);
+        struct PixelBufferHead head;
+        memcpy(&head, _cacheData.bytes, sizeof(head));
+//        NSLog(@">>>size %d, w %d, h %d, cache %lu", head.size, head.width, head.height, (unsigned long)_cacheData.length);
 
-            int32_t bufferSize = (int32_t)head.size;
+        int32_t bufferSize = (int32_t)head.size;
 
-            if (self->_cacheData.length >= sizeof(head) + bufferSize) {
-                void *buffer = malloc(bufferSize);
-                memcpy(buffer, self->_cacheData.bytes + sizeof(head), bufferSize);
+        while (_cacheData.length >= sizeof(head) + bufferSize) {
+            void *buffer = malloc(bufferSize);
+            memcpy(buffer, _cacheData.bytes + sizeof(head), bufferSize);
 
-                CVPixelBufferRef pixelBuffer = [XWReplayConvertTool createCVPixelBufferRefFromNV12buffer:buffer head:head];
-                free(buffer);
+            CVPixelBufferRef pixelBuffer = [XWReplayConvertTool createCVPixelBufferRefFromNV12buffer:buffer head:head];
+            free(buffer);
 
-                int32_t leftOverSize = (int32_t)self->_cacheData.length - sizeof(head) - bufferSize;
-                if (leftOverSize > 0) {
-                    void *leftOver = malloc(leftOverSize);
-                    memcpy(leftOver, self->_cacheData.bytes + sizeof(head) + bufferSize, leftOverSize);
-                    self->_cacheData = [NSMutableData dataWithBytes:leftOver length:leftOverSize];
-                    free(leftOver);
-                } else {
-                    self->_cacheData = [NSMutableData data];
-                }
-
-                if ([self.delegate respondsToSelector:@selector(transport:didReceivedBuffer:info:)]) {
-                    NSDictionary *info = @{
-                        @"size" : @(bufferSize),
-                        @"width" : @(head.width),
-                        @"height" : @(head.height),
-                        @"timeStamp" : @(head.timeStamp),
-                    };
-                    [self.delegate transport:self didReceivedBuffer:pixelBuffer info:info];
-                }
-                CVPixelBufferRelease(pixelBuffer);
+            int32_t leftOverSize = (int32_t)_cacheData.length - sizeof(head) - bufferSize;
+            if (leftOverSize > 0) {
+                void *leftOver = malloc(leftOverSize);
+                memcpy(leftOver, _cacheData.bytes + sizeof(head) + bufferSize, leftOverSize);
+                _cacheData = [NSMutableData dataWithBytes:leftOver length:leftOverSize];
+                free(leftOver);
+            } else {
+                _cacheData = [NSMutableData data];
             }
 
-            [sock readDataWithTimeout:-1 tag:0];
+            if ([self.delegate respondsToSelector:@selector(transport:didReceivedBuffer:info:)]) {
+                NSDictionary *info = @{
+                    @"size" : @(bufferSize),
+                    @"width" : @(head.width),
+                    @"height" : @(head.height),
+                    @"timeStamp" : @(head.timeStamp),
+                };
+                [self.delegate transport:self didReceivedBuffer:pixelBuffer info:info];
+            }
+            CVPixelBufferRelease(pixelBuffer);
         }
-    });
+
+        [sock readDataWithTimeout:-1 tag:0];
+    }
 }
 
 @end
