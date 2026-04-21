d6ed35ab6cd0f6c26c9ba2fac489813920323b66
qiangjindong <qiangjindong@163.com>
2026-04-21 15:30:32 +0800

    fix: 修复未使用变量

--- common/cpp/include/flutter_video_renderer.h
+++ common/cpp/include/flutter_video_renderer.h
@@ -57,8 +57,10 @@ class FlutterVideoRenderer
   mutable std::shared_ptr<uint8_t> rgb_buffer_;
   mutable std::mutex mutex_;
   RTCVideoFrame::VideoRotation rotation_ = RTCVideoFrame::kVideoRotation_0;
+#if defined(FLUTTER_WEBRTC_RENDERER_PERF)
   int64_t perf_e2e_sum_ms_ = 0;
-  int64_t perf_e2e_frame_count_ = 0;
+  int64_t perf_e2e_frame_count_ = 0;  
+#endif
 };
 
 class FlutterVideoRendererManager {
